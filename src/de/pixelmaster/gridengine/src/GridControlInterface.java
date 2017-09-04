054d000f0c346f2e3e63b01264a843e340dbc6a1
xcg1406 <adrian.schneble@fiduciagad.de>
2017-09-04 16:11:18 +0200

    Setup eclipse project with basic project structure

---
+++ src/de/pixelmaster/gridengine/src/GridControlInterface.java
@@ -0,0 +1,8 @@
+package de.pixelmaster.gridengine.src;
+
+import java.awt.Color;
+
+public interface GridControlInterface
+{
+    public void setColor(int x, int y, Color color);
+}
---
+++ src/de/pixelmaster/gridengine/test/GridControl.java
@@ -0,0 +1,16 @@
+package de.pixelmaster.gridengine.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GridControl
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
